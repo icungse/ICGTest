df3be9d6742d007707b995ef7a0af2d6ca8c3f5c
Icung <icung@Icungs-MacBook-Air.local>
2023-06-29 21:58:27 +0700

    implement pod

---
+++ ICGTest/ICGTest.swift
@@ -0,0 +1,20 @@
+//
+//  ICGTest.swift
+//  ICGTest
+//
+//  Created by Icung on 29/06/23.
+//
+
+import Foundation
+
+public final class ICGTest {
+    let name = "ICGTest"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
