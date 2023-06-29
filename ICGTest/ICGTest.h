9a383f68962808e4879e030c8411a2de3a27eee9
Icung <icung@Icungs-MacBook-Air.local>
2023-06-29 21:50:23 +0700

    initial commit

---
+++ ICGTest/ICGTest.h
@@ -0,0 +1,18 @@
+//
+//  ICGTest.h
+//  ICGTest
+//
+//  Created by Icung on 29/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ICGTest.
+FOUNDATION_EXPORT double ICGTestVersionNumber;
+
+//! Project version string for ICGTest.
+FOUNDATION_EXPORT const unsigned char ICGTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICGTest/PublicHeader.h>
+
+
---
+++ ICGTestExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICGTestExample
+//
+//  Created by Icung on 29/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
